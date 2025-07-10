63dfa0a71ac1e51cf65544b7e469124fe44c8726
Yakir-Itzhak <yakir.itzhak2@gmail.com>
2025-07-10 17:28:42 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main{
+    void printMessage() {
+        System.out.println("This is MyClass!");
+    }
+
+
+
+    public static void main(String[] args) {
+        // Create an instance of the class
+        Main myObject = new Main();
+
+        // Call the method to print the message
+        myObject.printMessage();
+        System.out.println("Hello, World!");
+    }
+}
